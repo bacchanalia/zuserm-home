bacchanalia/zuserm-home

commit c0a7ef730751eb4df5ac1a1b6afbdd7a08095022

taffybar[Fcrondyn] updated to work with new fcron wrappers, fcrontab location

.config/taffybar/lib/Fcrondyn.hs
@@ -17,14 +17,13 @@ fcrondynW = clickableLeft cmd =<< labelW getMarkup
 getMarkup = do
   now <- getCurrentTime
   tz <- getCurrentTimeZone
-  running <- isRunning "fcron"
-  if not running then void $ runCommand "sudo fcron" else return ()
-  fcrondynOut <- readProc ["sudo", "fcrondyn", "-x", "ls"]
+  runCommand "sudo fcron-start"
+  fcrondynOut <- readProc ["sudo", "fcron-ls"]
   return $ parseAndFormat now tz fcrondynOut
 
 cmd = ""
       ++ "term \""
-      ++ "vim $HOME/.fcrontab; fcronreset; echo OK; read STDIN"
+      ++ "vim $HOME/install/root-files/etc/fcrontab; gksudo $HOME/install/bin/sync-root; sudo fcronreset; echo OK; read STDIN"
       ++ "\""
 
 parseAndFormat now tz fcrondynOut = rows now tz (namedJobs okJobs)

commit 1096303aaf6067cf6848374d9b93dfe6e047556b

install[Fcrondyn] fixed a bug introduced in c0a7ef730751eb4df5ac1a1b6afbdd7a08095022. I was calling runCommand instead of system becayse that was what was in the code before. This wasn't a problem because it was guarded behind a call to isRunning, but I revealed it when I moved the check into the fcron-start wrapper.

.config/taffybar/lib/Fcrondyn.hs
@@ -2,7 +2,7 @@ module Fcrondyn(fcrondynW) where
 import Clickable (clickableLeft)
 import Label (labelW)
 import System.IO
-import System.Process(runCommand, readProcessWithExitCode)
+import System.Process(readProcessWithExitCode, system)
 import Data.Maybe (catMaybes, fromMaybe, fromJust, isJust)
 import Control.Monad (void)
 
@@ -17,7 +17,7 @@ fcrondynW = clickableLeft cmd =<< labelW getMarkup
 getMarkup = do
   now <- getCurrentTime
   tz <- getCurrentTimeZone
-  runCommand "sudo fcron-start"
+  system "sudo fcron-start"
   fcrondynOut <- readProc ["sudo", "fcron-ls"]
   return $ parseAndFormat now tz fcrondynOut
 