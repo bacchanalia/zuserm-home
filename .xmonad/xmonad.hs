453b667486a09c8b48f8eec9f2ae3c5b9a0bd2ae
Mike Zuser <mikezuser@gmail.com>
2014-11-02 23:01:25 -0500

    xmonad: increase pidgin ping timeout

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -66,10 +66,10 @@ addStartUps conf = conf { startupHook = startupHook', manageHook = manageHook' }
   where
     osw ws cmd cond = tell [(ws, cmd, cond)]
     startups = execWriter $ do
-        osw "1" "execPing --timeout 6 pidgin" $ className =? "Pidgin"
-        osw "8" "gnucash"                     $ className =? "Gnucash"
-        osw "9" "icedove"                     $ className =? "Icedove"
-        osw "9" "term vim TODO/TODO"          $ command   =~? "TODO/TODO"
+        osw "1" "execPing --timeout=15 pidgin" $ className =? "Pidgin"
+        osw "8" "gnucash"                      $ className =? "Gnucash"
+        osw "9" "icedove"                      $ className =? "Icedove"
+        osw "9" "term vim TODO/TODO"           $ command   =~? "TODO/TODO"
     startupHook' = do
         forM startups $ \( _, cmd, cond) -> spawnUnless cond cmd
         startupHook conf
