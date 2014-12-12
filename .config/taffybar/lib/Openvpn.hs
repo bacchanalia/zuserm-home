5cb97cf01da40552a93d8f8cd68605ba5aa2cba2
Elliot Wolk <elliot.wolk@gmail.com>
2014-12-12 17:49:49 -0500

    taffybar: openvpn=>sslvpn

--- .config/taffybar/lib/Openvpn.hs
+++ .config/taffybar/lib/Openvpn.hs
@@ -1,14 +1,27 @@
 module Openvpn(openvpnW) where
 import Clickable (clickableLeft)
-import Label (labelW)
-import Utils (isRunning, fg)
+import Label (labelW, mainLabel)
+import Utils (procSuccess, fg)
+import Data.List (intercalate)
 
-clickCmd = "sudo sslvpn toggle"
+import System.Environment (getArgs)
+import Control.Monad (when)
 
-vpnMarkup = do
-  vpnOn <- isRunning "openvpn"
-  let text = if vpnOn then "yes" else "off"
+main = do
+  args <- getArgs
+  when (length args /= 1) (error "Usage: Openvpn CONF_NAME")
+  let confName = args !! 0
+  mainLabel $ openvpnReader confName
+openvpnW confName = do
+  label <- labelW $ openvpnReader confName
+  clickableLeft (sslCmdText "toggle" confName) label
+
+sslCmd :: String -> String -> [String]
+sslCmd cmd confName = ["sudo", "sslvpn", cmd, confName]
+sslCmdText :: String -> String -> String
+sslCmdText cmd confName = intercalate " " $ sslCmd cmd confName
+
+openvpnReader confName = do
+  vpnOn <- procSuccess $ sslCmd "running" confName
   let color = if vpnOn then "green" else "red"
-  return $ fg color $ "vpn\n" ++ text
-
-openvpnW = clickableLeft clickCmd =<< labelW vpnMarkup
+  return $ fg color $ take 3 confName
