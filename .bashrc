teleshoes/wolke-home-config

bashrc: remove -ZF from feh

.bashrc
@@ -113,7 +113,7 @@ alias :r='. /etc/profile; . ~/.bashrc;'
 function e            { email.pl --print "$@"; }
 function vol          { pulse-vol "$@"; }
 function j            { fcron-job-toggle "$@"; }
-function f            { feh -ZF "$@"; }
+function f            { feh "$@"; }
 function snapshot     { backup --snapshot "$@"; }
 function qgroups-info { backup --info --quick --sort-by=size "$@"; }
 function dus          { du -s * | sort -g "$@"; }

bacchanalia/zuserm-home

Conflicts:
  .bashrc

.bashrc
@@ -84,7 +84,7 @@ function ls           { command ls --color=auto "$@"; }
 function l            { ls -alh "$@"; }
 function ll           { l "$@"; }
 function ld           { l -d "$@"; }
-function i            { feh -FZ "$@" ; }
+function i            { feh "$@" ; }
 function xmb          { xmonad-bindings "$@"; }
 function g            { git "$@"; }
 function gs           { g s "$@"; }